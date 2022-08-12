1e194b77736dcb31e1e61630da0a7cacde7de229
sherena99 <hiroshimasherena@gmail.com>
2022-08-12 12:03:08 +0530

    Complete chat between one client and user

--- Client/src/controller/ClientTwoController.java
+++ Client/src/controller/ClientTwoController.java
@@ -1,15 +1,59 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class ClientTwoController {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientTwoController implements Initializable {
     public AnchorPane clientTwoContext;
     public TextArea txtTextArea;
     public TextField txtTextField;
 
-    public void sendBtnOnAction(ActionEvent actionEvent) {
+    static DataInputStream dataInputStream;
+    static DataOutputStream dataOutputStream;
+    Socket socket = null;
+    String messageIn = "";
+
+
+    public void sendBtnOnAction(ActionEvent actionEvent) throws IOException {
+        String reply = "";
+        reply = txtTextField.getText();
+        txtTextArea.appendText(("\t\t\t\t\t\t\t\tClientOne :" + reply.trim()));
+        dataOutputStream.writeUTF(reply);
+        txtTextField.setText("");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket("localhost", 5000);
+                    dataInputStream = new DataInputStream(socket.getInputStream());
+                    dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+
+                    while (!messageIn.equals("end")) {
+                        messageIn = dataInputStream.readUTF();
+                        txtTextArea.appendText("\nServer :" + messageIn.trim() + "\n");
+                    }
+
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
     }
 }
--- Server/src/ServerPageController.java
+++ Server/src/ServerPageController.java
@@ -1,14 +1,57 @@
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class ServerPageController {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ServerPageController implements Initializable {
     public AnchorPane serverPageContext;
     public TextArea txtTextArea;
     public TextField txtTextField;
 
+    static ServerSocket serverSocket;
+    static Socket socket;
+    static DataInputStream dataInputStream;
+    static DataOutputStream dataOutputStream;
 
-    public void sendBtnOnAction(ActionEvent actionEvent) {
+    String messageIn = "";
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        new Thread(()->{
+            try {
+                serverSocket = new ServerSocket(5000);
+                System.out.println("Server Started Waiting for client! .....");
+                socket=serverSocket.accept();
+                System.out.println("Client Accepted! .......");
+
+                dataInputStream=new DataInputStream(socket.getInputStream());
+                dataOutputStream=new DataOutputStream(socket.getOutputStream());
+
+                while (!messageIn.equals("end")){
+                    messageIn=dataInputStream.readUTF();
+                    txtTextArea.appendText("\nClient:" + messageIn.trim()+"\n");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+
+    public void sendBtnOnAction(ActionEvent actionEvent) throws IOException {
+        String text = txtTextField.getText();
+        txtTextArea.appendText("\t\t\t\t\t\t\t\tServer :" +text.trim());
+        dataOutputStream.writeUTF(text);
+        txtTextField.setText("");
+
     }
 }
